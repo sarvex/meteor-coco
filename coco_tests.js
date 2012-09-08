86f985439edc60da78b519c2fc6ca43d740e0373
Andrew Kelley <superjoe30@gmail.com>
2012-09-08 17:56:34 -0400

    initial version

---
+++ coco_tests.js
@@ -0,0 +1,3 @@
+Tinytest.add("coco - presence", function(test) {
+  test.isTrue(Meteor.__COCO_PRESENT);
+});
---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  summary: "Javascript dialect with fewer braces and semicolons"
+});
+
+var coco = require('coco');
+var fs = require('fs');
+
+Package.register_extension(
+  "co", function (bundle, source_path, serve_path, where) {
+    serve_path = serve_path + '.js';
+
+    var contents = fs.readFileSync(source_path);
+    var options = {bare: true};
+    contents = new Buffer(coco.compile(contents.toString('utf8'), options));
+    // XXX report coco compile failures better?
+
+    bundle.add_resource({
+      type: "js",
+      path: serve_path,
+      data: contents,
+      where: where
+    });
+  }
+);
+
+Package.on_test(function (api) {
+  api.add_files(['coco_tests.co', 'coco_tests.js'],
+                ['client', 'server']);
+});
